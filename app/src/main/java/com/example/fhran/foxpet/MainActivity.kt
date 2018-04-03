0a1cc971466cad8cc83ca4854595b82b59d363f8
Luxusz <fhrancisco.rios@gmail.com>
2018-04-03 00:54:54 -0300

    Login
    Toma datos ingresados y comprueba que el password tenga un largo igual o mayor de 8 caracteres

--- app/src/main/java/com/example/fhran/foxpet/MainActivity.kt
+++ app/src/main/java/com/example/fhran/foxpet/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.fhran.foxpet
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnLogin.setOnClickListener(){
+            val algo : Int = txtPassword.length()
+            if(algo >= 8){
+                Toast.makeText(this, "Hola ${txtUsername.text}", Toast.LENGTH_LONG).show()
+            }
+            else{
+                Toast.makeText(this, "Password no es correcto!", Toast.LENGTH_LONG).show()
+
+            }
+
+
+
+        }
+
     }
 }
